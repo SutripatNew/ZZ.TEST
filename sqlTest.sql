cea6d189c04f82f98621b83c394d36db07304e8d
“TNT-SutripatNg” <sutripat.ng@thainamthip.co.th>
2025-12-23 15:39:06 +0700

    CR#1113_revamp_20251223

--- sqlTest.sql
+++ sqlTest.sql
@@ -11,3 +11,11 @@ FROM [dbo].[Test_Source]
 -- CR#11111_Test2
 SELECT *
 FROM [dbo].[Test_Source]
+
+-- CR#1113_Test3
+SELECT *
+FROM [dbo].[Test_Source]
+
+-- CR#1113_Test4
+SELECT *
+FROM [dbo].[Test_Target]
